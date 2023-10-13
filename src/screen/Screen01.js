77816328182f3da915611ab114b4691a1676c938
nhatlinh <nhatlinhdev201@gmail.com>
2023-10-13 08:43:09 +0700

    update UI screen1

---
+++ src/screen/Screen01.js
@@ -0,0 +1,148 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import Rating from '@mui/material/Rating';
+import HelpIcon from '@mui/icons-material/Help';
+
+const Screen01 = () => {
+
+    const [srcImageProductMain, setSrcImageProductMain] = useState('../image/vs_black.png')
+
+    return (
+        <View style={styles.container}>
+            <View
+                style={{
+                    justifyContent : 'center',
+                    alignItems : 'center'
+                }}
+            >
+                <Image
+                    source={require('../image/vs_black.png')}
+                    style={styles.imageProductMain}
+                />
+            </View>
+            <Text style={{
+                textAlign: 'left',
+                fontSize: 17,
+            }}>
+                Điện Thoại Vsmart Joy 3 - Hàng chính hãng
+            </Text>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginTop: 15
+                }}
+            >
+                <Rating
+                    name="size-large"
+                    defaultValue={5}
+                    size="large"
+                    style={{
+                        marginRight: 10
+                    }}
+                />
+                <Text>(Xem 828 đánh giá)</Text>
+            </View>
+            <View style={{
+                flexDirection: 'row',
+                marginTop: 15
+            }}>
+                <Text
+                    style={{
+                        marginRight: 40,
+                        fontSize: 18,
+                        fontWeight: 700
+                    }}
+                >
+                    1.790.000 đ
+                </Text>
+                <Text
+                    style={{
+                        marginRight: 40,
+                        fontSize: 18,
+                        fontWeight: 600,
+                        textDecorationLine: 'line-through'
+                    }}
+                >
+                    1.790.000 đ
+                </Text>
+            </View>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    marginTop: 15
+                }}
+            >
+                <Text
+                    style={{
+                        color: 'red',
+                        fontSize: 15,
+                        fontWeight: 700,
+                        marginRight: 20
+                    }}
+                >
+                    Ở ĐÂU RẺ HƠN HOÀN TIỀN
+                </Text>
+                <TouchableOpacity
+                    onPress={() => {
+
+                    }}
+                >
+                    <HelpIcon />
+                </TouchableOpacity>
+            </View>
+            <TouchableOpacity
+                style={{
+                    alignItems: 'center',
+                    borderWidth: 1,
+                    borderColor: 'gray',
+                    borderRadius: 8,
+                    padding: 10,
+                    marginTop: 20
+                }}
+            >
+                <Text style={{
+                    fontSize: 15,
+                    color: 'black',
+                    fontWeight: 600,
+
+                }}>
+                    4 MẪU CHỌN MÀU       >
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={{
+                    alignItems: 'center',
+                    borderWidth: 1,
+                    borderColor: 'gray',
+                    borderRadius: 8,
+                    padding: 10,
+                    marginTop: 20,
+                    backgroundColor : '#CA1536'
+                }}
+            >
+                <Text style={{
+                    fontSize: 15,
+                    color: 'white',
+                    fontWeight: 600,
+
+                }}>
+                    CHỌN MUA
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default Screen01
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        padding: 20
+    },
+    imageProductMain: {
+        width: 300,
+        height: 350
+    }
+})
