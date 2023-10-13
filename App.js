c902446fe6bfe6bf0d85579d0dd84e3b51637ce3
nhatlinh <nhatlinhdev201@gmail.com>
2023-10-13 08:55:11 +0700

    update UI screen2

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Screen01 from './src/screen/Screen01';
+import Screen2 from './src/screen/Screen2';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Screen01/>
+      {/* <Screen2/> */}
     </View>
   );
 }
---
+++ src/screen/Screen2.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyleSheet, Text, View,Image } from 'react-native'
+
+const Screen2 = () => {
+    return (
+        <View>
+            <View
+                style={{
+                    
+                }}
+            >
+                <Image
+                    source={require('../image/vs_blue.png')}
+                    style ={{
+                        width : 112,
+                        height : 132
+                    }}
+                />
+            </View>
+        </View>
+    )
+}
+
+export default Screen2
+
+const styles = StyleSheet.create({})
