dd119eb81d77318397b178d79ea90e39e3965030
nhatlinh <nhatlinhdev201@gmail.com>
2023-10-19 21:11:25 +0700

    update final : create stack for screen app by navigation

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Screen01 from './src/screen/Screen01';
 import Screen2 from './src/screen/Screen2';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Screen4 from './src/screen/Screen4';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Screen01/>
-      {/* <Screen2/> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Screen1'>
+        <Stack.Screen name='Screen1' component={Screen01} options={{ headerShown: false }}/>
+        <Stack.Screen name='Screen2' component={Screen2} options={{ headerTitle:"Chọn loại sản phẩm" }}/>
+        <Stack.Screen name='Screen4' component={Screen4} options={{headerTitle : "Xác nhận thông tin"}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
